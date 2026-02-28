ef16b3ee6c20efc15c2dd92a01ae4453eb45fb25
John Garside <skipper@Johns-MacBook-Pro.local>
2026-02-27 20:26:18 -0600

    update

--- Backend/app.py
+++ Backend/app.py
@@ -1,12 +1,14 @@
 #Flask Backend 
-#new test
+
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route("/")
-def home():
-    return "Hello, World!"
+@app.route('/')
+def index():
+    return 'Server is running!'
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
